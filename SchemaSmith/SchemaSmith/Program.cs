4ccdaf66fb9165bb571d33c7c37d9f0f13adc074
Donovan Bergin <Donovan.Bergin@jbhunt.com>
2022-09-08 12:23:58 -0500

    Removing unused code.

--- SchemaSmith/SchemaSmith/Program.cs
+++ SchemaSmith/SchemaSmith/Program.cs
@@ -11,45 +11,12 @@ internal class Program
     static async Task<int> Main(string[] args)
     {
         Console.OutputEncoding = System.Text.Encoding.Unicode;
-        
-        var fileOption = new Option<FileInfo?>(
-            name: "--file",
-            description: "The file to read and display on the console.");
-        
-        var delayOption = new Option<int>(
-            aliases: new[] {"--delay", "-d" },
-            description: "Delay between lines, specified as milliseconds per character in a line.",
-            getDefaultValue: () => 42);
-
-        var fgcolorOption = new Option<ConsoleColor>(
-            aliases: new[] {"--fgcolor", "-c" },
-            description: "Foreground color of text displayed on the console.",
-            getDefaultValue: () => ConsoleColor.White);
-
-        var lightModeOption = new Option<bool>(
-            aliases: new[] {"--light-mode", "-l" },
-            description: "Background color of text displayed on the console: default is black, light mode is white.");
 
         var rootCommand = new RootCommand("Sample app for System.CommandLine");
-
-        var readCommand = new Command("read", "Read and display the file")
-        {
-            fileOption,
-            delayOption,
-            fgcolorOption,
-            lightModeOption
-        };
         
-        rootCommand.AddCommand(readCommand);
         rootCommand.AddCommand(Lint.LintCommand);
         rootCommand.AddCommand(Script.ScriptCommand);
         rootCommand.AddCommand(RunScript.RunScriptCommand);
-        
-        readCommand.SetHandler(async (file, delay, fgcolor, lightMode) =>
-            {
-                await ReadFile(file!, delay, fgcolor, lightMode);
-            },
-            fileOption, delayOption, fgcolorOption, lightModeOption);
 
         return await rootCommand.InvokeAsync(args);
     }
