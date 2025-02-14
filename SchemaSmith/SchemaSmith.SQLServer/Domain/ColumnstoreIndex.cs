5d216a1ec023cc32338367706e7af02d26ec903e
Donovan Bergin <kaiserbergin@gmail.com>
2025-02-14 11:46:17 -0600

    get some col store indexes yo

---
+++ SchemaSmith/SchemaSmith.SQLServer/Domain/ColumnstoreIndex.cs
@@ -0,0 +1,10 @@
+﻿namespace SchemaSmith.SQLServer.Domain;
+
+public class ColumnstoreIndex
+{
+    public string Name { get; set; } = null!;
+    public bool IsClustered { get; set; } = true;
+    public List<string> Columns { get; set; } = new();
+    public List<string> Order { get; set; } = new();
+    public string? AdditionalClauses { get; set; }
+}
